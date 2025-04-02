feb6b99b155469fd61a3042ae50ef30ab1e37b84
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-04-02 12:57:29 +0300

    feat: build Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+interface ButtonProps {
+  children: React.ReactNode;
+  textColor: string;
+  bgColor: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+function Button({ children, textColor, bgColor, onClick }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className="button"
+      style={{ color: textColor, backgroundColor: bgColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
   return <h1>Steps</h1>;
 }
